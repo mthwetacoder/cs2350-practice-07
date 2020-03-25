b932b756d5a63bcf83af91471dcba6f6b42a5875
mthwetacoder <54649998+mthwetacoder@users.noreply.github.com>
2020-03-25 15:28:11 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,85 +4,45 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
-//TODO
-let c = document.querySelector('canvas');
-ctx = c.getContext('2d');
-
-const clear = function() {
-    ctx.clearRect(0, 0, 500, 395);
-}
-
-function make_cube() {
-    clear();
-
-    let x = 10;
-    x = document.getElementById('x').value;
-
-    ctx.strokeStyle = document.getElementById('c').value;
-    ctx.fillStyle = document.getElementById('c').value;
-    ctx.strokeRect(10, 10, x * 25, x * 25);
-    ctx.strokeRect(10 + (x * 25) / 2, 10 + (x * 25) / 2, x * 25, x * 25);
-
-    ctx.beginPath();
-    ctx.moveTo(10, 10);
-    ctx.lineTo(10 + (x * 25) / 2, 10 + (x * 25) / 2);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(10 + x * 25, 10);
-    ctx.lineTo(10 + (x * 25) / 2 + (x * 25), 10 + (x * 25) / 2);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(10, 10 + x * 25);
-    ctx.lineTo(10 + (x * 25) / 2, 10 + (x * 25) / 2 + (x * 25));
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(10 + x * 25, 10 + x * 25);
-    ctx.lineTo(10 + (x * 25) / 2 + (x * 25), 10 + (x * 25) / 2 + (x * 25));
-    ctx.stroke();
-}
-
-function randomDots() {
-    clear(); //clears the whole canvas, then does below
-    num_o_dots = document.getElementById('amount').value;
-    for (let i = 0; i < num_o_dots; i++) {
-        let r = Math.floor(Math.random() * 256);
-        let g = Math.floor(Math.random() * 256);
-        let b = Math.floor(Math.random() * 256);
-        let color = "rgb(" + r + "," + g + "," + b + ")";
-
-        ctx.fillStyle = color;
-        ctx.fillRect(Math.random() * 500, Math.random() * 395, 5, 5);
+const randomBox = function() {
+    let rnum = Math.random()
+    if(rnum < 0.49){
+        document.write('<div class="red box"></div>')
+    } else if(rnum > 0.51) {
+        document.write('<div class="blue box"></div>')
+    } else {
+        document.write('<div class="green box"></div>')
     }
 }
 
-function choosing_canvas(){
-    clear(); //clears the whole canvas, then does below
-
-    choice = document.getElementById('Canvas_type');
-    if (choice.value == "undefined"){
-        document.getElementById('cube_options').style.display = "none";
-        document.getElementById('dots_options').style.display = "none";
-    } 
-    else if (choice.value == "cube") {
-        clear();
-        make_cube();
-        document.getElementById('dots_options').style.display = "none";
-        document.getElementById('cube_options').style.display = "inline";
-
-        document.getElementById('x').onchange = make_cube;
-        document.getElementById('c').onchange = make_cube;
-    } else if (choice.value == "random_dots") {
-        clear();
-        document.getElementById('cube_options').style.display = "none";
-        document.getElementById('dots_options').style.display = "inline";
-
-        randomDots();
-        document.getElementById('amount').onchange = randomDots;
+function createBoxesFor(num_o_boxes) {
+    for(let i = 0; i < num_o_boxes; i++) {
+        randomBox()
     }
 }
-// document.body.onload = choosing_canvas;
-//document.body.onload = randomDots;
-document.getElementById('Canvas_type').onchange = choosing_canvas;
+
+function createBoxesWhile(num_o_boxes) {
+    let j = 0
+    while (j < num_o_boxes) {
+        randomBox()
+        j++
+    }
+}
+
+function createBoxesDoWhile(num_o_boxes) {
+    let k = 0
+    while (k < num_o_boxes) {
+        randomBox()
+        k++
+    }
+}
+
+const square = function(x) {
+    return x * x
+}
+
+// alert("The square of 2 is " + square(2))
+
+createBoxesFor(10)
+createBoxesWhile(10)
+createBoxesDoWhile(10)
